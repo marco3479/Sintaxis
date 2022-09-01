b913a8adfe5e6a800de9f7187a85efde832cdc3d
marco3479 <marcomongaloiii@gmail.com>
2022-09-01 12:03:04 -0400

    updated og:image:type meta tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,6 +46,7 @@ const Home: NextPage = () => {
             <meta property="og:image:width" content="300"/>
             <meta property="og:image:height" content="300"/>
             <meta property="og:type" content="website" />
+            <meta property="og:image:type" content="image/png"/>
           </Head>
           <div className='relative h-full w-full'>
             <div className='h-full w-full'>
